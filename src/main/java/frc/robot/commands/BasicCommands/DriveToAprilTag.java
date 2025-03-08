f639ca62119e8959c688f5ba40a1ebdc27ba6833
3313 Admin <3313coach@gmail.com>
2025-03-08 11:06:32 -0600

    driveToAprilTag changes mostly

--- src/main/java/frc/robot/commands/BasicCommands/DriveToAprilTag.java
+++ src/main/java/frc/robot/commands/BasicCommands/DriveToAprilTag.java
@@ -14,28 +14,35 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.StateMachine;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
 
 public class DriveToAprilTag extends Command {
 
   private final CommandSwerveDrivetrain swerveDrive;
+  private final StateMachine stateMachine;
+  private String limelight;
 
-  private final SwerveRequest.FieldCentricFacingAngle driveRequest = new FieldCentricFacingAngle()
-  .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.Velocity);
+  // private final SwerveRequest.FieldCentricFacingAngle driveRequest = new FieldCentricFacingAngle()
+  // .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.Velocity);
+  private final SwerveRequest.RobotCentric driveRequest = new SwerveRequest.RobotCentric()
+    .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.Velocity);
 
   private final PIDController xController;
   private final PIDController yController;
 
-  private double offsetX = 0;
-  private double offsetY = 0;
-  private double offsetZ = 0;
+  private double offsetX = 0.012;
+  private double offsetY = -0.055;
   private double rot = 0;
 
-  private final double kP = 2;
+  private final double kP = .1;
 
-  public DriveToAprilTag(CommandSwerveDrivetrain swerveDrive) {
+  private double txError = 0.15, tyError = 1;
+
+  public DriveToAprilTag(CommandSwerveDrivetrain swerveDrive, StateMachine stateMachine) {
     this.swerveDrive = swerveDrive;
+    this.stateMachine = stateMachine;
 
     this.xController = new PIDController(kP, 0.0, 0.0);
     this.yController = new PIDController(kP, 0.0, 0.0);
@@ -48,32 +55,39 @@ public class DriveToAprilTag extends Command {
     // Reset PID controllers
     xController.reset();
     yController.reset();
+    if(stateMachine.isScoreLeft())
+      limelight = Constants.Limelight.FRONT;
+    else{
+      limelight = Constants.Limelight.RIGHT;
+        offsetX *= -1;
+      }
 
-    driveRequest.HeadingController = new PhoenixPIDController(4, 0, 0);
-    driveRequest.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
+    // driveRequest.HeadingController = new PhoenixPIDController(4, 0, 0);
+    // driveRequest.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
 
-    LimelightHelpers.setFiducial3DOffset(Constants.Limelight.RIGHT, offsetX, offsetY, offsetZ);
+    LimelightHelpers.setFiducial3DOffset(Constants.Limelight.RIGHT, offsetX, offsetY, 0);
   }
 
   @Override
   public void execute()
   {
-    double tx = LimelightHelpers.getTX(Constants.Limelight.RIGHT); // Horizontal offset to offset POI
-    double ty = LimelightHelpers.getTY(Constants.Limelight.RIGHT); // Vertical offset to offset POI
-    boolean tv = LimelightHelpers.getTV(Constants.Limelight.RIGHT);
+    double tx = LimelightHelpers.getTX(limelight); // Horizontal offset to offset POI
+    double ty = LimelightHelpers.getTY(limelight); // Vertical offset to offset POI
+    boolean tv = LimelightHelpers.getTV(limelight);
+
+    tx += offsetX;
+    ty += offsetY;
 
     if(tv)
     {
-      double robotYVelocity = ty * kP;
-      double robotXVelocity = tx * kP;
+      double robotYVelocity = yController.calculate(tx, 0);
+      double robotXVelocity = xController.calculate(ty, 0);
 
       Rotation2d currentHeading = swerveDrive.getState().Pose.getRotation();
-      double fieldXVelocity = robotXVelocity * currentHeading.getCos() - robotYVelocity * currentHeading.getSin();
-      double fieldYVelocity = robotXVelocity * currentHeading.getSin() + robotYVelocity * currentHeading.getCos();
 
-      driveRequest.VelocityX = fieldXVelocity;
-      driveRequest.VelocityY = fieldYVelocity;
-      driveRequest.TargetDirection = Rotation2d.fromDegrees(rot);
+      driveRequest.VelocityY = robotYVelocity;
+      driveRequest.VelocityX = -robotXVelocity;
+      // driveRequest.TargetDirection = Rotation2d.fromDegrees(rot);
 
       swerveDrive.setControl(driveRequest);
     }
@@ -85,10 +99,13 @@ public class DriveToAprilTag extends Command {
 
 
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    
+  }
 
   @Override
   public boolean isFinished() {
-    return false;
+    return LimelightHelpers.getTX(Constants.Limelight.RIGHT) <= txError && 
+      LimelightHelpers.getTY(Constants.Limelight.RIGHT) <= tyError;
   }
 }
