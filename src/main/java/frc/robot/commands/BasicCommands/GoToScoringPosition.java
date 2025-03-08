c73b06bfd4f8f13a7dc1d1a6d87c8afcd2a861c3
SiriusRPO <70858755+SiriusRPO@users.noreply.github.com>
2025-03-08 00:25:02 -0600

    Added drive to april tage with new shit

--- src/main/java/frc/robot/commands/BasicCommands/GoToScoringPosition.java
+++ src/main/java/frc/robot/commands/BasicCommands/GoToScoringPosition.java
@@ -21,18 +21,30 @@ public class GoToScoringPosition extends Command {
   private final CommandSwerveDrivetrain drivetrain;
   private final StateMachine stateMachine;
   private Pose2d targetPose;
+  private boolean scoreLeft;
+  private int tagID;
+
+  double blueReefX = 4.48945, blueReefY = 4.0259;//DO NOT CHANGE EVER
+  double redReefX = 13.0683, redReefY = 4.0259; //DO NOT CHANGE EVER
+
+  double blueXOffSet = 0, blueYOffset = 0;
+  double redXOffSet = 0, redYOffset = 0;
+  double reefRadius = 1.06045;
+
+  //adjustments tips
+  //if the distance to the reef is off adjust the radius
+  //if the robot of shift all the way around adjust offset for reef
 
   private final PIDController xController;
   private final PIDController yController;
-  private final SwerveRequest.FieldCentric driveRequest = new SwerveRequest.FieldCentric()
-      .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.OpenLoopVoltage);
 
-  private static final double POSITION_TOLERANCE = 0.0125; // meters
-  private static final double ROTATION_TOLERANCE = Math.toRadians(2); // radians
+  private static final double POSITION_TOLERANCE = 0.01; // meters
+  private static final double ROTATION_TOLERANCE = Math.toRadians(1); // radians
   private static final double MAX_SPEED = 4.0; // meters/sec
   private static final double MAX_ANGULAR_RATE = Math.toRadians(270); // 270°/s in rad/s (~4.71 rad/s)
+
   //private final AprilTagFieldLayout fieldLayout;
-  private final SwerveRequest.FieldCentricFacingAngle snapDrive = new FieldCentricFacingAngle()
+  private final SwerveRequest.FieldCentricFacingAngle driveRequest = new FieldCentricFacingAngle()
   .withDriveRequestType(com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType.Velocity);
 
   public GoToScoringPosition(CommandSwerveDrivetrain drivetrain, StateMachine stateMachine) {
@@ -50,13 +62,41 @@ public class GoToScoringPosition extends Command {
 
   @Override
   public void initialize() {
-    snapDrive.HeadingController = new PhoenixPIDController(4, 0, 0);
-    this.targetPose = ScoreConditioningCalculator();
-
-    snapDrive.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
+    driveRequest.HeadingController = new PhoenixPIDController(4, 0, 0);
+    driveRequest.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
     xController.reset();
     yController.reset();
+    if(stateMachine.isScoreLeft()){scoreLeft = true;}
+    else{scoreLeft = false;}
+
+    if(scoreLeft)
+    {
+      LimelightHelpers.RawFiducial[] fiducials = LimelightHelpers.getRawFiducials(Constants.Limelight.RIGHT);
+      if(fiducials.length > 0)
+      {
+        double tagIdDouble = fiducials[0].id;
+        tagID = (int) tagIdDouble;
+      }
+      else
+      {
+        return;
+      }
+    }
+    else
+    {
+      LimelightHelpers.RawFiducial[] fiducials = LimelightHelpers.getRawFiducials(Constants.Limelight.FRONT);
+      if(fiducials.length > 0)
+      {
+        double tagIdDouble = fiducials[0].id;
+        tagID = (int) tagIdDouble;
+      }
+      else
+      {
+        return;
+      }
+    }
     
+    targetPose = RobotPositionCalculator(tagID);
   }
 
   @Override
@@ -72,11 +112,11 @@ public class GoToScoringPosition extends Command {
 
       xVel = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, xVel));
       yVel = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, yVel));
-      System.out.println("degrees :" + targetPose.getRotation());
-      drivetrain.setControl(snapDrive
-      .withVelocityX(xVel)
-      .withVelocityY(yVel)
-      .withTargetDirection(targetPose.getRotation()));
+
+      drivetrain.setControl(driveRequest
+        .withVelocityX(xVel)
+        .withVelocityY(yVel)
+        .withTargetDirection(targetPose.getRotation()));
     }
   }
 
@@ -96,66 +136,40 @@ public class GoToScoringPosition extends Command {
     return xError < POSITION_TOLERANCE && yError < POSITION_TOLERANCE;
   }
 
-  Pose2d ScoreConditioningCalculator()
+  Pose2d RobotPositionCalculator(int tagID)
   {
-    double reefX; //meters
-    double reefY; //meters
+    Pose2d tagInfo = GetTagInfo(tagID);
 
-    double radius = 1.60046; //meters1.2
-    double angleoffset = 5.66;
-    if(DriverStation.getAlliance().get() == DriverStation.Alliance.Blue)
-    {
-      reefX = 4.84505;
-      reefY = 4.1;
-    }
-    else if(DriverStation.getAlliance().get() == DriverStation.Alliance.Red)
-    {
-      reefX = 4.84505;
-      reefY = 4.0259;
-    }
-    else
-    {
-      end(true);
-      return null;
-    }
+    double littleTheta = Math.atan(0.1643/reefRadius);
+    littleTheta = littleTheta + tagInfo.getRotation().getDegrees();
+    double hype = Math.sqrt((reefRadius * reefRadius) + (0.1642 * 0.1643));
+    double theta = tagInfo.getRotation().getDegrees() - littleTheta;
+    
+    double x = hype * Math.cos(Math.toRadians(theta));
+    double y = hype * Math.sin(Math.toRadians(theta));
+    
+    Pose2d robotTargetPos = new Pose2d(blueReefX + x, blueReefY + y, Rotation2d.fromDegrees(tagInfo.getRotation().getDegrees() - 180));
+    return robotTargetPos;
+  }
 
-    double targetX = 0;
-    double targetY = 0;
+  Pose2d GetTagInfo(int tagID)
+  {
+    double xPos = 0, yPos = 0, rot = 0;
 
-    if(stateMachine.isScoreLeft())
-    {
-      angleoffset = angleoffset * -1;
-    }
-    double tagAngle = 0;
-    // Get the tag ID for the visible April tag
-    int tagId = (int)LimelightHelpers.getFiducialID(Constants.Limelight.FRONT);
-    //Optional<Pose3d> tagPose = fieldLayout.getTagPose(tagId);
-    //fieldLayout.getTagPose(tagId);
-    //System.out.println("tagPose: " + Units.radiansToDegrees(tagPose.get().getRotation().getAngle()));
-    if(tagId ==  18)
-      tagAngle = 180;
-    else if (tagId == 19)
-      tagAngle = 120;
-    else if(tagId ==  20)
-      tagAngle = 60;
-    else if (tagId == 21)
-      tagAngle = 0;
-    else if (tagId == 22)
-      tagAngle = -60;
-    else if(tagId ==  17)
-      tagAngle = -120;
-    else
-      // Abandon ship!
-      return null;      
-    
-    double deltaangle = tagAngle + angleoffset;
-    targetX = radius * Math.cos(Math.toRadians(deltaangle));
-    targetY = radius * Math.sin(Math.toRadians(deltaangle));
-    
-    targetX = reefX + targetX;
-    targetY = reefY + targetY; 
-    
-    return new Pose2d(targetX, targetY, Rotation2d.fromDegrees(tagAngle));
-  
+    if(tagID == 6){xPos = 530.49; yPos = 130.17; rot = 300;}
+    else if(tagID == 7){xPos = 546.87 ; yPos = 158.50; rot = 0;}
+    else if(tagID == 8){xPos = 530.49 ; yPos = 186.83; rot = 60;}
+    else if(tagID == 9){xPos = 497.77; yPos = 186.83; rot = 120;}
+    else if(tagID == 10){xPos = 481.39; yPos = 158.50; rot = 180;}
+    else if(tagID == 11){xPos = 497.77; yPos = 130.17; rot = 240;}
+    else if(tagID == 17){xPos =160.39; yPos = 130.17; rot = 240;}
+    else if(tagID == 18){xPos = 144.00; yPos = 158.50; rot = 180;}
+    else if(tagID == 19){xPos = 160.39; yPos = 186.83; rot = 120;}
+    else if(tagID == 20){xPos = 193.10 ; yPos = 186.83; rot = 60;}
+    else if(tagID == 21){xPos = 209.49; yPos = 158.50; rot = 0;}
+    else if(tagID == 22){xPos = 193.10; yPos = 130.17; rot = 300;}
+    else{xPos = drivetrain.getState().Pose.getX(); yPos = drivetrain.getState().Pose.getY(); rot = drivetrain.getState().Pose.getRotation().getDegrees();}
+
+    return new Pose2d(xPos, yPos, Rotation2d.fromDegrees(rot));
   }
 }
